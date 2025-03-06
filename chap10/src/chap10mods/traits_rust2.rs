b9ef20cfd8157c0586831bcf41e798eb7d667373
Ibrahim <98814268+ibrahimhaji0708@users.noreply.github.com>
2025-03-06 21:43:41 +0530

    display + partialord

---
+++ chap10/src/chap10mods/traits_rust2.rs
@@ -0,0 +1,22 @@
+struct Pair<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Pair<T> {
+    fn new(x: T, y: T) -> Self {
+        Self { x, y }
+    }
+}
+
+impl<T: Display + PartialOrd> Pair<T> {
+    fn cmp_display(&self) {
+        if self.x >= self.y {
+            println!("the largets member is: {}", self.x);
+        } else {
+            println!("the largest member is y: {}", self.y);
+        }
+    }
+}
+
+fn rust_code_more() {}
--- chap10/src/main.rs
+++ chap10/src/main.rs
@@ -3,5 +3,6 @@ pub mod chap10mods;
 fn main() {
     // chap10mods::number_list::number_lists();
     // chap10mods::gen_types::gen_type();
-    chap10mods::traits_rust::trait_code();
+    // chap10mods::traits_rust::trait_code();
+    chap10mods::traits_rust::trait_code_more();
 }
