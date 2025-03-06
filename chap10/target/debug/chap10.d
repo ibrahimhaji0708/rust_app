7e8d5f513611a53f4639526c3a90c3c13cc6b0c7
Ibrahim <98814268+ibrahimhaji0708@users.noreply.github.com>
2025-03-06 10:31:21 +0530

    chp 10 traits, lifetimjes mods

---
+++ chap10/target/debug/chap10.d
@@ -0,0 +1,1 @@
+/home/ibrahim/code/Rust_lang/rust_app/chap10/target/debug/chap10: /home/ibrahim/code/Rust_lang/rust_app/chap10/src/main.rs
---
+++ chap10/target/debug/deps/chap10-2100307859638671.d
@@ -0,0 +1,5 @@
+/home/ibrahim/code/Rust_lang/rust_app/chap10/target/debug/deps/chap10-2100307859638671: src/main.rs
+
+/home/ibrahim/code/Rust_lang/rust_app/chap10/target/debug/deps/chap10-2100307859638671.d: src/main.rs
+
+src/main.rs:
