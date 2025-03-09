794bc67289b72f9691fa4c75e562d78af0c7746d
Ibrahim <98814268+ibrahimhaji0708@users.noreply.github.com>
2025-03-09 16:36:26 +0530

    generics, traits, lifetimes

--- chap10/src/chap10mods/lifetimes_rust.rs
+++ chap10/src/chap10mods/lifetimes_rust.rs
@@ -1,15 +1,15 @@
-pub fn lifetimes_code() {
-    let str1 = String::from("abcd");
-    let str2 = String::from("xyz");
-
-    let result = longest(str1.as_str(), str2.as_str());
-    println!("longest: {result}");
-}
-
-fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+use std::fmt::Display;
+fn longest_with_ann<'a, T>(
+    x: &'a str,
+    y: &'a str,
+    ann: T,
+) -> &'a str where T: Display {
+    println!("announcement: {}", ann);
     if x.len() > y.len() {
         x
     } else {
         y
     }
 }
+
+pub fn lifetimes_code() {}
