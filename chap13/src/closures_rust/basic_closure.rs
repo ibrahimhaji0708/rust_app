a92e7302f3f5c28860d89fe4f1e8d8345efa8e69
Ibrahim <98814268+ibrahimhaji0708@users.noreply.github.com>
2025-03-27 14:56:33 +0530

    practice_closures

--- chap13/src/closures_rust/basic_closure.rs
+++ chap13/src/closures_rust/basic_closure.rs
@@ -3,3 +3,26 @@ pub fn addn() {
     let res = add(5, 5);
     println!("the result is {}", res);
 }
+
+pub fn capture_by_reference() {
+    let x = 5;
+    let print_x = || println!("x is {}", x);
+    print_x();
+}
+
+pub fn capture_by_value() {
+    let x = String::from("hello");
+    let takes_ownership = || {
+        println!("x inside closure: {} ", x);
+    };
+    takes_ownership();
+}
+
+pub fn capture_by_mut_reference() {
+    let mut x = 5;
+    let mut change_x = || {
+        x += 1;
+    };
+    change_x();
+    println!("x after closure {}", x);
+}
--- chap13/src/main.rs
+++ chap13/src/main.rs
@@ -1,7 +1,7 @@
 pub mod closures_rust;
 
 fn main() {
-    // closures_rust::basic_closure::addn();
+    closures_rust::basic_closure::capture_by_mut_reference();
     // closures_rust::closures::shirt();
-    closures_rust::rect_closure::rect_closures();
+    // closures_rust::rect_closure::rect_closures();
 }
